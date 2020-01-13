b77ab188528521b8692d47d5af046702000faf85
Jesse Dodge <dodgejesse@gmail.com>
2020-01-13 13:26:09 -0800

    updated the README.md, made the location of where to put the data more clear.

--- plot.py
+++ plot.py
@@ -1,9 +1,10 @@
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-from . import expected_max_performance
+import expected_max_performance
 
 def one_plot(data, data_name, logx=False, plot_errorbar=True, avg_time=0, performance_metric="accuracy"):
     # to set default values
@@ -72,18 +73,27 @@ def save_plot(data_name, logx, plot_errorbar, avg_time):
     plt.savefig(name, dpi=300)
 
 
+def main():
+    # The N validation accuracies can go here:
+    example_valid_perf = [.8, .75, .9, .24, .45, .6, .65, .67, .49, .49, .59, .52, .78,
+                          .84, .63, .42, .85, .76, .68, .6]
+
+    data = expected_max_performance.samplemax(example_valid_perf)
+
+    # In most cases only one plot will be necessary.
+    # These examples are meant to show what the plots look like with and without log-scaling the X-axis and
+    # shading +/- the standard error (similar to standard deviation).
+    one_plot(data, "Experiment Name", logx=False, plot_errorbar=False, avg_time=0)
+    one_plot(data, "Experiment Name", logx=True, plot_errorbar=False, avg_time=0)
+    one_plot(data, "Experiment Name", logx=False, plot_errorbar=True, avg_time=0)
+    one_plot(data, "Experiment Name", logx=True, plot_errorbar=True, avg_time=0)
+
+    # If the average runtime for the experiments is included, the X-axis will be time instead of trials.
+    one_plot(data, "Experiment Name", logx=False, plot_errorbar=False, avg_time=10)
+    one_plot(data, "Experiment Name", logx=True, plot_errorbar=False, avg_time=10)
+    one_plot(data, "Experiment Name", logx=False, plot_errorbar=True, avg_time=10)
+    one_plot(data, "Experiment Name", logx=True, plot_errorbar=True, avg_time=10)
+
 
 if __name__ == "__main__":
-    example_valid_perf = np.random.uniform(0,1, 20)
-    data = expected_max_performance.samplemax(example_valid_perf)
-    one_plot(data, "SST", logx=False, plot_errorbar=False, avg_time=0)
-    one_plot(data, "SST", logx=True, plot_errorbar=False, avg_time=0)
-    one_plot(data, "SST", logx=False, plot_errorbar=True, avg_time=0)
-    one_plot(data, "SST", logx=True, plot_errorbar=True, avg_time=0)
-
-    one_plot(data, "SST", logx=False, plot_errorbar=False, avg_time=10)
-    one_plot(data, "SST", logx=True, plot_errorbar=False, avg_time=10)
-    one_plot(data, "SST", logx=False, plot_errorbar=True, avg_time=10)
-    one_plot(data, "SST", logx=True, plot_errorbar=True, avg_time=10)
-
-
+    main()
